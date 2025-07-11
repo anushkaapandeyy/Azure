168d59822637aa63922c9dc6e41f29c9af4d1497
Anushka Pandey <anushkaa.pandey1@gmail.com>
2025-07-11 19:19:47 +0530

    Fix GitHub Actions workflow: Configure remote backend and proper authentication

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
-    backend "azurerm"{
-        resource_group_name = "terraform-state-rg"
-        storage_account_name = "storageaccounttfaap"
-        container_name = "tfstate"
-        key = "terraform.tfstate"
-        }
- }
+  backend "azurerm" {
+    resource_group_name  = "terraform-state-rg"
+    storage_account_name = "storageaccounttfaap"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
